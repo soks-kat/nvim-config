2529256df42db55856ebe33f8ed2c02b80b92884
nullptr <eliesbertran@gmail.com>
2024-10-15 01:16:07 +0200

    Compile treesitters with zig if on windows

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -1,23 +1,21 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    -- ensure_installed = { 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    -- auto_install = false,
-    auto_install = false,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
+  config = function()
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    if vim.fn.has 'win32' == 1 then
+      require('nvim-treesitter.install').compilers = { 'zig' }
+    end
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup { -- Sets main module to use for opts
+      -- ensure_installed = { 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      additional_vim_regex_highlighting = true,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      rainbow = { enable = true },
+    }
+  end,
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
