ede90070eb87b089f59cd74fcf14d5466804ab39
nullptr <eliesbertran@gmail.com>
2024-10-15 12:57:29 +0200

    Better handling of toggleterm

--- lua/setup/toggleterm.lua
+++ lua/setup/toggleterm.lua
@@ -3,8 +3,14 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup {
-      open_mapping = [[<leader>o]],
+      -- open_mapping = [[<leader>o]],
       start_in_insert = true,
     }
+    vim.keymap.set('n', '<leader>o', ':ToggleTerm<Cr>')
+    local exitTerm = function()
+      vim.cmd ':ToggleTerm'
+    end
+
+    vim.keymap.set('t', '<esc><esc>', exitTerm)
   end,
 }
