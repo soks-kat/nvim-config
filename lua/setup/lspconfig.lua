462c4e211dbe592a554b2a77bd49b217aa948500
nullptr <eliesbertran@gmail.com>
2024-10-18 19:46:21 +0200

    Added command to show diagnostics

--- lua/setup/lspconfig.lua
+++ lua/setup/lspconfig.lua
@@ -94,6 +94,8 @@ return {
         --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+        map('<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', 'Open diagnostics in a floating window')
+
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
